1e9c8e02d90c9a07d9333554edbab769996dc2e6
Talha Waheed <62260739+Talha0077@users.noreply.github.com>
2021-01-09 07:57:37 +0500

    Add files via upload

---
+++ IMDb Mini Database/cmake-build-debug/CMakeFiles/DSA_Mini_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSA_Mini_Project.dir/main.cpp.obj"
+  "DSA_Mini_Project.exe"
+  "DSA_Mini_Project.exe.manifest"
+  "DSA_Mini_Project.pdb"
+  "libDSA_Mini_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSA_Mini_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
